fc6de8debbdda7cffcdb7e222fd9528341004e5c
junriladlawan <143474950+junriladlawan@users.noreply.github.com>
2026-01-12 21:04:14 +0800

    Add files via upload

---
+++ IPT102Adlawan/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task<IEnumerable<T>> GetListAsync(
+            string procedure,
+            DynamicParameters? parameters = null,
+            CancellationToken cancellationToken = default);
+
+        Task<T?> GetSingleAsync(
+            string procedure,
+            DynamicParameters? parameters = null,
+            CancellationToken cancellationToken = default);
+
+        Task<int> SaveAsync(
+            string procedure,
+            DynamicParameters? parameters = null,
+            CancellationToken cancellationToken = default);
+    }
+}
---
+++ IPT102Adlawan/Repository/Repository.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T>
+    {
+        private readonly string _dbConnection;
+
+        protected BaseRepository(string dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        protected IDbConnection OpenConnection()
+        {
+            return new SqlConnection(_dbConnection);
+        }
+
+        public async Task<IEnumerable<T>> GetListAsync(
+            string procedure,
+            DynamicParameters? parameters = null,
+            CancellationToken cancellationToken = default)
+        {
+            using var connection = OpenConnection();
+
+            return await connection.QueryAsync<T>(
+                procedure,
+                parameters,
+                commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<T?> GetSingleAsync(
+            string procedure,
+            DynamicParameters? parameters = null,
+            CancellationToken cancellationToken = default)
+        {
+            using var connection = OpenConnection();
+
+            return await connection.QueryFirstOrDefaultAsync<T>(
+                procedure,
+                parameters,
+                commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<int> SaveAsync(
+            string procedure,
+            DynamicParameters? parameters = null,
+            CancellationToken cancellationToken = default)
+        {
+            using var connection = OpenConnection();
+
+            return await connection.ExecuteAsync(
+                procedure,
+                parameters,
+                commandType: CommandType.StoredProcedure);
+        }
+    }
+}
